bc058f1839f89655e6281859649489eb7ab98d77
sho1cchi <ichikawa.s.417@gmail.com>
2019-08-06 04:49:03 +0900

    configuration gem/cocoon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require activestorage
+
 //= require rails-ujs
-  //= require turbolinks
-  //= require jquery
-  //= require bootstrap-sprockets
-  //= require_tree .
+//= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
+//= require_tree .
+//= require activestorage
+//= require cocoon
