b24de0648f254ef4d45b9d048725a766c86f62ed
sho1cchi <ichikawa.s.417@gmail.com>
2019-08-11 02:23:59 +0900

    fix conditional branch of user, enum is_appending

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,9 +7,9 @@ class Review < ApplicationRecord
 	#dependent: :deleteはしない。postscriptはnoteの一部。
 	#reviewあってのpostscriptなのでhas_one
 
-	enum is_appending:{
-		追記を希望しない:0,
-		追記を希望する:1,
-		希望した追記にリアクション済み:2
+	enum is_appending: {
+		追記を希望しない: 0,
+		追記を希望する: 1,
+		希望した追記にリアクション済み: 2
 	}
 end
